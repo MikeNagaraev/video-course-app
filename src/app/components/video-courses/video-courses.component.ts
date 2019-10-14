76c3f524f46b3970040c2f528f476d57beaac57f
Mikhail Naharayeu <Mikhail_Naharayeu@epam.com>
2019-10-14 09:32:58 +0300

    Task-1: structure, dumb components, entities, tslint

---
+++ src/app/components/video-courses/video-courses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-courses-list',
+  templateUrl: './video-courses.component.html',
+  styleUrls: ['./video-courses.component.less']
+})
+export class VideoCoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
